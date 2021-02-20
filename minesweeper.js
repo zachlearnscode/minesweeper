67a5ebc1c48c420bfd3583d4662064ae749b5961
Zach <zmcnulty980@gmail.com>
2021-02-20 11:23:33 -0600

    Added simple win, lose, continue methods. Cleaned up mounted()

--- minesweeper.js
+++ minesweeper.js
@@ -18,6 +18,7 @@ var app = new Vue({
         {content: "", clicked: false}
       ]
     ],
+    remainingBoard: this.unclickedCells,
     wrapper: {
       width: '6rem',
       height: '6rem',
@@ -32,70 +33,82 @@ var app = new Vue({
       alignItems: 'center',
       width: '2rem',
       height: '2rem',
-      background: '#D6D6D6'
+      background: '#D6D6D6',
+      fontFamily: "VT323, monospace"
     },
-    unclicked: {
+    fontSize: {
+      fontSize: "1.7rem"
+    },
+    hidden: {
       borderStyle: "outset"
     },
-    clicked: {
+    revealed: {
       border: '0.5px solid darkgray'
     }
   },
-  beforeMount() {
+  computed: {
+
+  },
+  methods: {
+    remainingCells() {
+      let remaining = [];
+      let board = this.board.flat();
+
+      for (let i = 0; i < board.length; i++) {
+        if (!board[i].clicked) {
+          remaining.push(board[i]);
+        }
+      }
+
+      return remaining;
+    },
+    reveal(cell) {
+      cell.clicked = true;
+      
+      if (cell.content === "💣") {
+        return console.log("You Lose")
+      } else {
+        let remainingCells = this.remainingCells();
+        for (let c = 0; c < remainingCells.length; c++) {
+          if (remainingCells[c].content !== "💣") {
+            return console.log("Keep Playing")
+          }
+        }
+        return console.log("You Win!")
+      }
+    },
+    boundsCheck(row, col) {
+      if (row > -1 && row < this.board.length &&
+          col > -1 && col < this.board[row].length) {
+            return this.bombCheck(row, col);
+      }
+    },
+    bombCheck(row, col) {
+      if (this.board[row][col].content === "💣") {
+        return 1;
+      }
+    }
+  },
+  mounted() {
     for (let row = 0; row < this.board.length; row++) {
       for (let col = 0; col < this.board[row].length; col++) {
-        let nearbyBombs = 0;
 
-        if (this.board[row][col].content !== "💣") {
-          //above
-          if (row - 1 > -1) {
-            if (this.board[row - 1][col].content === "💣") {
-              nearbyBombs++;
-            }
+        let curr = this.board[row][col];
+        if (curr.content === "") {
+          let nearbyBombs = 0;
+
+          nearbyBombs += this.boundsCheck(row - 1, col) || 0;
+          nearbyBombs += this.boundsCheck(row + 1, col) || 0;
+          nearbyBombs += this.boundsCheck(row, col - 1) || 0;
+          nearbyBombs += this.boundsCheck(row, col + 1) || 0;
+          nearbyBombs += this.boundsCheck(row - 1, col - 1) || 0;
+          nearbyBombs += this.boundsCheck(row - 1, col + 1) || 0;
+          nearbyBombs += this.boundsCheck(row + 1, col - 1) || 0;
+          nearbyBombs += this.boundsCheck(row + 1, col + 1) || 0;
+
+          if (nearbyBombs !== 0) {
+            curr.content = String(nearbyBombs);
           }
-          //below
-          if (row + 1 < this.board.length) {
-            if (this.board[row + 1][col].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          //left
-          if (col - 1 > -1) {
-            if (this.board[row][col - 1].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          //right 
-          if (col + 1 < this.board[row].length) {
-            if (this.board[row][col + 1].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          //above left
-          if (row - 1 > -1 && col - 1 > -1) {
-            if (this.board[row - 1][col - 1].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          //above right
-          if (row - 1 > -1 && col + 1 < this.board[row].length) {
-            if (this.board[row - 1][col + 1].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          //below left 
-          if (row + 1 < this.board.length && col - 1 > -1) {
-            if (this.board[row + 1][col - 1].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          //below right
-          if (row + 1 < this.board.length && col + 1 < this.board[row].length) {
-            if (this.board[row + 1][col + 1].content === "💣") {
-              nearbyBombs++;
-            }
-          }
-          this.board[row][col].content = String(nearbyBombs)
         }
       }
     }
